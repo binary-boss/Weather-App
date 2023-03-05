93a8431b666faeebb746d45a5d5e41a38a226484
adityagit <aditya@tray.io>
2023-03-05 23:33:12 +0530

    fixed api path

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -16,84 +16,81 @@
 
 */
 
-const APIkey = '390a56a4e5743b65dabe7b47f1c4c0f0';
-const submitBtn = document.querySelector('#submit');
+const APIkey = "390a56a4e5743b65dabe7b47f1c4c0f0";
+const submitBtn = document.querySelector("#submit");
 
-let lat = '';
-let lon = '';
+let lat = "";
+let lon = "";
 
-submitBtn.addEventListener('click', getLocation);
+submitBtn.addEventListener("click", getLocation);
 
 async function getLocation(e) {
   e.preventDefault();
 
-  const city = document.getElementById('city').value;
+  const city = document.getElementById("city").value;
 
   //Big if block to catch empty input field error, prevent from attemping to connect to API when empty
   //prompt user to enter city.
   if (city) {
-              try {
-                  //API call 1
-                  //Take user input city to get latitude and longitude cordinates
-                  const locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},us&limit=5&appid=${APIkey}`);
+    try {
+      //API call 1
+      //Take user input city to get latitude and longitude cordinates
+      const locationResponse = await fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city},us&limit=5&appid=${APIkey}`
+      );
 
-                  const data = await locationResponse.json();
-                  const cityLocation = Object.values(data);
-                
-                  //Get latitude and longitude from data obj
-                  lat = (cityLocation[0]['lat']);
-                  lon = (cityLocation[0]['lon']);
+      const data = await locationResponse.json();
+      const cityLocation = Object.values(data);
 
-                
-                  //API call 2
-                  //Send lat and lon cordinates and get weather data
-                  const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=524901&units=imperial&appid=${APIkey}`);
-                  const data1 = await weatherResponse.json();
-                  const myCity = Object.values(data1);
-              
-                  //Call render function to get weather info from array Obj
-                  //Then add pre-build html block from render's return function with weather data inserted.
-                  const APIdata = render(myCity);
-                  document.getElementById("APIinfo").innerHTML = APIdata;
-                
-              } catch (error) {
+      //Get latitude and longitude from data obj
+      lat = cityLocation[0]["lat"];
+      lon = cityLocation[0]["lon"];
 
-              const  errormsg = `<div id='error'>Invalid input. Please enter city.</div>`;
-                document.getElementById("APIinfo").innerHTML = errormsg;
+      //API call 2
+      //Send lat and lon cordinates and get weather data
+      const weatherResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=524901&units=imperial&appid=${APIkey}`
+      );
+      const data1 = await weatherResponse.json();
+      const myCity = Object.values(data1);
 
-              };
-   
-    } else {
-      const msg = `<div id='error'>Input field cannot be empty.<br/>Please enter a city.</div>`;
-      document.getElementById("APIinfo").innerHTML = msg;
-  };
-} 
-
+      //Call render function to get weather info from array Obj
+      //Then add pre-build html block from render's return function with weather data inserted.
+      const APIdata = render(myCity);
+      document.getElementById("APIinfo").innerHTML = APIdata;
+    } catch (error) {
+      const errormsg = `<div id='error'>Invalid input. Please enter city.</div>`;
+      document.getElementById("APIinfo").innerHTML = errormsg;
+    }
+  } else {
+    const msg = `<div id='error'>Input field cannot be empty.<br/>Please enter a city.</div>`;
+    document.getElementById("APIinfo").innerHTML = msg;
+  }
+}
 
 const render = function (arrObj) {
+  //turn object into an array of arrays
+  const myCity = Object.values(arrObj);
 
-//turn object into an array of arrays
-const myCity = Object.values(arrObj);
+  const responseCode = myCity[0];
+  const myNewCity = myCity[4]["name"];
+  const userCountry = myCity[4]["country"];
 
-const responseCode = myCity[0]; 
-const myNewCity = (myCity[4]['name']);
-const userCountry = (myCity[4]['country']);
+  //const todaysDate = (myCity[3][0]['dt_txt']);
+  const todaysDate = Date().toString().split(" ");
+  const convertedDate = convertDate(todaysDate);
 
-//const todaysDate = (myCity[3][0]['dt_txt']);
-const todaysDate = Date().toString().split(' ');
-const convertedDate = convertDate(todaysDate);
+  const temp = Math.round(myCity[3][0]["main"]["temp"]);
 
-const temp = Math.round((myCity[3][0]['main']['temp']));
+  let weather = myCity[3][0]["weather"][0]["description"];
+  weather = capitalizeFirstLetter(weather);
 
-let weather = (myCity[3][0]['weather'][0]['description'])
-weather = capitalizeFirstLetter(weather);
+  const highTemp = Math.round(myCity[3][0]["main"]["temp_max"]);
 
-const highTemp = Math.round((myCity[3][0]['main']['temp_max']));
+  const lowTemp = Math.round(myCity[3][0]["main"]["temp_min"]);
 
-const lowTemp = Math.round((myCity[3][0]['main']['temp_min']));
-
-//Insert data into HTML divs. Then send back to caller
-let myString = `    
+  //Insert data into HTML divs. Then send back to caller
+  let myString = `    
           <div id="userCity">${myNewCity}, ${userCountry}</div>
           <div id="date">${convertedDate}</div>
 
@@ -103,48 +100,46 @@ let myString = `
           <div class="myAlign">Weather: ${weather}</div>
           <div class="myAlign">Temp Range: ${highTemp}&#8457; / ${lowTemp}&#8457;</div>`;
 
-//Access code
-//console.log(`${responseCode}`);
-return myString; 
-} ;
+  //Access code
+  //console.log(`${responseCode}`);
+  return myString;
+};
 
 //Convert date format
 function convertDate(todaysDate) {
-
   //Deconstruct numeric date
   let [dayElem, monthElem, dateELem, yearElem] = todaysDate;
   //console.log(dayElem + monthElem + dateELem + yearElem);
 
-//months obj use for reference for month abbreviations  
-  const months = 
-    {
-      jan: "january",
-      feb: "february",
-      mar: "march",
-      apr: "april",
-      may: "may",
-      jun: "june",
-      jul: "july",
-      aug: "august",
-      sept: "september",
-      oct: "october",
-      nov: "november",
-      dec: "december"
-    };
+  //months obj use for reference for month abbreviations
+  const months = {
+    jan: "january",
+    feb: "february",
+    mar: "march",
+    apr: "april",
+    may: "may",
+    jun: "june",
+    jul: "july",
+    aug: "august",
+    sept: "september",
+    oct: "october",
+    nov: "november",
+    dec: "december",
+  };
 
-    //access month object to get non-abbreviated month
-    //send month to get first letter capitalezed
-    monthElem = capitalizeFirstLetter(months[monthElem.toLowerCase()]);
-  
-    //save day with comma
-    dayElem = dayElem + ', ';
-  
+  //access month object to get non-abbreviated month
+  //send month to get first letter capitalezed
+  monthElem = capitalizeFirstLetter(months[monthElem.toLowerCase()]);
+
+  //save day with comma
+  dayElem = dayElem + ", ";
+
   //Put all part of the date string together
   const finalDate = `${dayElem} ${monthElem} ${dateELem} ${yearElem}`;
 
   return finalDate;
-};
+}
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
-};
+}
